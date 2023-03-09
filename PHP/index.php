28e8b5dde14f2a1da5669040fdf018be812fbd3a
Victor Armando <aven132ink@gmail.com>
2023-03-09 04:19:37 -0500

    se arreglan problemas en el codigo php

--- PHP/index.php
+++ PHP/index.php
@@ -1,13 +1,15 @@
 <?php
 
-require_once 'car.php';
-require_once 'account.php';
+require_once('car.php');
+require_once('account.php');
+require_once('uberPool.php');
+require_once('uberBlack.php');
+require_once('uberVan.php');
+require_once('uberX.php');
 
-$car = new Car(new Account("Victor Armando", "ABCD1234"), 'ABCD1234');
+$car = new UberX(new Account("Victor Armando", "ABCD1234"), 'ABCD1234', 'mercedes', 'benz');
 
-$car->id = '5';
-
-echo $car->id;
-echo $car->driver->name;
+echo $car->model;
+$car->printDataCar();
 
 ?>
